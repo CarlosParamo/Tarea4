054ae0ae26b47c55773764ec9a8a1891b9cc7a2e
CarlosParamo <capr24398@gmail.com>
2017-11-10 03:22:23 -0500

    first

---
+++ Ondas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE *infile;
+	infile = fopen("cond_ini_cuerda.dat", "w");
+	int N=129
+      
+	int Array[N];
+	int i;
+
+	for(i=0; i<N; i++)
+	{	
+		fscanf(infile,"%1d",&
+
